67e5d263ed3d357e438605077c53b2b40720d7e5
VikFe <vikomasu@gmail.com>
2025-12-16 16:40:17 +0500

    feat/frontend/11: Positioned the image and applied global styles for container and background

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -4,15 +4,15 @@ const AboutSection = () => {
     const [firstParagraph, ...otherParagraphs] = aboutData.text;
 
     return (
-        <section className="max-w-[390px] bg-gray-50 p-5">
+        <section className=" bg-gray-50 p-5">
             <h2 className="text-3xl font-bold mb-4">{aboutData.title}</h2>
 
-            <div className="bg-white rounded-lg shadow-md p-5 w-full">
-                <div className="flex items-start mb-4">
+            <div className="border border-gray-100 p-3 bg-white rounded-lg w-full">
+                <div className="flex items-start mb-3">
                     <p className="text-gray-700  flex-1">{firstParagraph}</p>
 
-                    <div className="flex-shrink-0 -mr-5 -mt-5">
-                        <img src={aboutData.image} alt="О нас" width={158} height={128} className="rounded-md shadow-sm flex-shrink-o" />
+                    <div className="shrink-0 -mr-3 -mt-3">
+                        <img src={aboutData.image} alt="О нас" width={158} height={128} className="rounded-tr-lg" />
                     </div>
                 </div>
 
