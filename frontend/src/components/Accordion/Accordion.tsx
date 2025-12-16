bd8dde39546032423b6ab4aa4674e51fa54b5743
Galina Bobrik <galagrinko@gmail.com>
2025-12-16 17:20:11 +0400

    fix/frontend/12: fix Button in shared and edit Accordion styles

--- frontend/src/components/Accordion/Accordion.tsx
+++ frontend/src/components/Accordion/Accordion.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import { MinusIcon } from '../../assets/icons/MinusIcon';
 import { PlusIcon } from '../../assets/icons/PlusIcon';
+import { Button } from '../../shared/ui';
 
 import accordionData from './data.json';
 
@@ -24,25 +25,32 @@ export const Accordion: React.FC = () => {
             </h2>
 
             <div className="border border-[#eeebf4] rounded-lg px-4 pb-4 max-w-[350px] bg-white">
-                {(accordionData as AccordionItemData[]).map((item, index) => {
-                    const isOpen = openItemIndex === index;
+                <div>
+                    {(accordionData as AccordionItemData[]).map((item, index) => {
+                        const isOpen = openItemIndex === index;
 
-                    return (
-                        <div key={index} className="border-b last:border-b-0 border-gray-200">
-                            <div className="flex justify-between items-center  cursor-pointer py-4" onClick={() => toggleItem(index)}>
-                                <h4 className="font-semibold text-sm text-gray-800 ">{item.question}</h4>
+                        return (
+                            <div key={index} className="border-b last:border-b-0 border-gray-200 ">
+                                <div className="flex justify-between items-center  cursor-pointer py-4" onClick={() => toggleItem(index)}>
+                                    <h4 className="font-semibold text-sm text-gray-800 ">{item.question}</h4>
 
-                                <span className="text-2xl font-light text-[#ff3131] transition-transform duration-300">
-                                    {isOpen ? <MinusIcon /> : <PlusIcon />}
-                                </span>
+                                    <span className="text-2xl font-light text-[#ff3131] transition-transform duration-300">
+                                        {isOpen ? <MinusIcon /> : <PlusIcon />}
+                                    </span>
+                                </div>
+
+                                <div
+                                    className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-96' : 'max-h-0'}`}
+                                >
+                                    <p className="py-4 text-[#26222b]  whitespace-pre-line max-w-[318px]  p5">{item.answer}</p>
+                                </div>
                             </div>
-
-                            <div className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-96' : 'max-h-0'}`}>
-                                <p className="py-4 text-[#26222b]  whitespace-pre-line max-w-[318px]  p5">{item.answer}</p>
-                            </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </div>
+                <Button color="darkPurple" size="sm">
+                    Все вопросы
+                </Button>
             </div>
         </div>
     );
